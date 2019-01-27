b3449204351661c59f857b0f0db7dfe1e4b0e83d
Mikhail Abramov <beavisabra@yandex.ru>
2019-01-27 22:13:18 +0300

    Init commit

---
+++ Haffmann/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Hamming
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(226, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(308, 217);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Хотел бы снять видос, да лень. Но ничего страшного, когда-нибудь получится. Не се" +
+    "годня, так завтра. Ну послезавтра прям совсем точно. По-любому, обещаю прям и кл" +
+    "янусь. Честно, слово пацана";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(226, 488);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(305, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Кодировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.richTextBox2.Location = new System.Drawing.Point(226, 260);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(305, 222);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.richTextBox3.Location = new System.Drawing.Point(540, 0);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(309, 217);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.richTextBox4.Location = new System.Drawing.Point(540, 262);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(306, 254);
+            this.richTextBox4.TabIndex = 5;
+            this.richTextBox4.Text = "";
+            this.richTextBox4.TextChanged += new System.EventHandler(this.richTextBox4_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(226, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ук";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(537, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(220, 528);
+            this.treeView1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 528);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Алгоритм Хаффмана";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Haffmann/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hamming
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Структура "узел H-дерева"
+        /// </summary>
+        public struct huffmannTreeNode
+        {
+            /// <summary>
+            /// Текст
+            /// </summary>
+            public String text;
+            /// <summary>
+            /// Двоичный код
+            /// </summary>
+            public String code;
+            /// <summary>
+            /// Частота встречаемости
+            /// </summary>
+            public int frequency;
+
+            public huffmannTreeNode(String t, String c, int f)
+            {
+                text = t;
+                code = c;
+                frequency = f;
+            }
+        };
+
+        /// <summary>
+        /// Частота встречаемости отдельных символов алфавита
+        /// </summary>
+        Dictionary<char, int> freqs = new Dictionary<char, int>();
+        /// <summary>
+        /// Исходное дерево
+        /// </summary>
+        List<huffmannTreeNode> source = new List<huffmannTreeNode>();
+        /// <summary>
+        /// Вспомогательное дерево
+        /// </summary>
+        List<huffmannTreeNode> newRes = new List<huffmannTreeNode>();
+        /// <summary>
+        /// Еще какое-то дерево
+        /// </summary>
+        List<huffmannTreeNode> tree = new List<huffmannTreeNode>();
+        
+        /// <summary>
+        /// Сортировка узлов дерева по убыванию
+        /// </summary>
+        void sortTree()
+        {
+            for (int index = 0; index < tree.Count - 1; index++)
+            {
+                for (int index2 = index; index2 < tree.Count; index2++)
+                {
+                    if (tree[index].frequency < tree[index2].frequency)
+                    {
+                        huffmannTreeNode buf = tree[index];
+                        tree[index] = tree[index2];
+                        tree[index2] = buf;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String text = richTextBox1.Text;
+
+            //Считаем частоту
+            for (int index = 0; index < richTextBox1.Text.Length; index++)
+            {
+                if (freqs.Keys.Contains(text[index]))
+                {
+                    freqs[text[index]]++;
+                }
+                else
+                {
+                    freqs.Add(text[index], 1);
+                }
+            }
+
+            //Начальное заполнение деревьев
+            foreach (KeyValuePair<char, int> Pair in freqs)
+            {
+                source.Add(new huffmannTreeNode(Pair.Key.ToString(), "", Pair.Value));
+                tree.Add(new huffmannTreeNode(Pair.Key.ToString(), "", Pair.Value));
+                newRes.Add(new huffmannTreeNode(Pair.Key.ToString(), "", Pair.Value));
+            }
+
+            //Переводим в байты
+            String textAsBytes = "";
+            BitArray btr = new BitArray(Encoding.ASCII.GetBytes(text));
+
+            for (int index = 0; index < btr.Count; index++)
+            {
+                textAsBytes += (btr[index] ? "1" : "0");
+            }
+
+            label1.Text = btr.Count.ToString() + "(" + freqs.Count.ToString() + ")";
+            richTextBox2.Text = textAsBytes;
+
+
+
+
+            //Строим дерево
+            while (tree.Count > 1)
+            {
+                sortTree();
+
+                for (int index = 0; index < source.Count; index++)
+                {
+                    if (tree[tree.Count - 2].text.Contains(source[index].text))
+                    {
+                        newRes[index] = new huffmannTreeNode(newRes[index].text, "0" + newRes[index].code, newRes[index].frequency);
+                    }
+                    else if (tree[tree.Count - 1].text.Contains(source[index].text))
+                    {
+                        newRes[index] = new huffmannTreeNode(newRes[index].text, "1" + newRes[index].code, newRes[index].frequency);
+                    }
+                }
+
+                tree[tree.Count - 2] = new huffmannTreeNode(tree[tree.Count - 2].text + tree[tree.Count - 1].text, "",
+                    tree[tree.Count - 2].frequency + tree[tree.Count - 1].frequency);
+                tree.RemoveAt(tree.Count - 1);
+            }
+
+            //Выводим алфавит на экран
+            treeView1.Nodes.Clear();
+            for (int index = 0; index < source.Count; index++)
+            {
+                treeView1.Nodes.Add(newRes[index].text + " (" + newRes[index].code + ")");
+            }
+
+            //Битовая последовательность с новыми кодами
+            for (int index = 0; index < text.Length; index++)
+            {
+                foreach (huffmannTreeNode htn in newRes)
+                {
+                    if (htn.text == text[index].ToString())
+                    {
+                        richTextBox4.Text += htn.code;
+                    }
+                }
+            }
+
+            label2.Text = richTextBox4.Text.Length.ToString();
+            
+            //Кодируем обратно в буквы
+            String encodedText = richTextBox4.Text;
+            for (int index = 0; index < encodedText.Length - 7; index += 8)
+            {
+                int code = 0;
+                for (int ind = 0; ind < 8; ind++)
+                {
+                    if (encodedText[index + ind] == '1')
+                    {
+                        code += Convert.ToInt32(Math.Pow(2, 7 - ind));
+                    }
+                }
+                richTextBox3.Text += (char)code;
+            }
+
+            //Дописываем алфавит в новую строку (чтобы раскодировать было можно)
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
